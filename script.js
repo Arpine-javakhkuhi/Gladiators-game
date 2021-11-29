7630b93a35c966f6bd35886d0f2f837d49618cbc
arpine_ktoyan <arpine.ktoyan@gmail.com>
2021-11-29 10:44:19 +0400

    feat(script.js): create attack interval for each gladiator

--- script.js
+++ script.js
@@ -7,10 +7,43 @@ class Gladiators {
     }
 }
 
+let gladiators = [];
+let intervals = [];
+
 start = () => {
     for (let i = 0; i < 10; i++) {
         gladiators.push(new Gladiators());
     }
+
+    startAttack();
+}
+
+function startAttack() {
+    for (let i = 0; i < gladiators.length; i++) {
+        attackProcess(gladiators[i], i);
+    }
+}
+
+async function attackProcess(gladiator, gladiatorInd) {
+    let speedToMs = (5 - gladiator.speed - 1) * 1000;
+
+    if (gladiators.length > 1) {
+        let attackInterval = setInterval(() => {
+            let randomGladiatorInd = chooseRandomGladiator(gladiators, gladiatorInd);
+            let randomGladiator = gladiators[randomGladiatorInd];
+
+        }, speedToMs);
+
+        intervals.push(attackInterval);
+    }
+};
+
+const chooseRandomGladiator = (gladiators, hittingGladiatorInd) => {
+    let randomRival;
+    do {
+        randomRival = Math.floor(Math.random() * gladiators.length);
+    } while (randomRival === hittingGladiatorInd)
+    return randomRival;
 }
 
 start();
